8da4ff7bebcd4bd39044bf723459598a6a317198
Pavel Senin <seninp@gmail.com>
2016-09-15 19:07:58 -0600

    standardizing the output of readers: kraken

--- R/merge_gottcha_assignments.R
+++ R/merge_gottcha_assignments.R
@@ -2,7 +2,7 @@
 NULL
 
 #' Merges two or more GOTTCHA-like taxonomical assignments. The input data frames are assumed to
-#' have the following columns: LEVEL, TAXA, and NORM_COV -- these will be used in the merge
+#' have the following columns: LEVEL, TAXA, and ABUNDANCE -- these will be used in the merge
 #' procedure, all other columns will be ignored.
 #'
 #' @param assignments A named list of assignments (the list element's name will be used as a
--- tests/testthat/test_load_kraken_assignment.R
+++ tests/testthat/test_load_kraken_assignment.R
@@ -8,13 +8,10 @@ dat <- data.frame(load_kraken_assignment(
 expect_that( dim(dat)[1], equals(2977) )
 
 #rows import test
-expect_that( dim(dat)[2], equals(6) )
+expect_that( dim(dat)[2], equals(4) )
 
 # col names test
-expect_that(colnames(dat)[1], matches("LEVEL"))
-
-#
-expect_that(colnames(dat)[ dim(dat)[2] ], matches("NORM_ROLLUP"))
+expect_that(colnames(dat), equals( c("LEVEL", "TAXA", "COUNT", "ABUNDANCE") ) )
 
 # [0.0] subset family
 family_table <- dplyr::filter( dat, LEVEL == "family")
@@ -24,8 +21,8 @@ ent_row <- dplyr::filter( family_table, TAXA == "Propionibacteriaceae")
 
 # [0.2] test
 # family Propionibacteriaceae 4123 8 31957
-expect_that(ent_row$ROLLUP, equals(4123))
-expect_that(ent_row$NORM_ROLLUP, equals(0.2913297))
+expect_that(ent_row$COUNT, equals(4123))
+expect_that(ent_row$ABUNDANCE, equals(0.2913297))
 
 #
 # test the failure
