2e26289c80993629388dfdee9c7123df4a6aa5dc
Pavel Senin <seninp@gmail.com>
2016-09-26 09:40:40 -0600

    fixing lintr warnings [ci skip]

--- tests/testthat/test_convert_to_taxa_table.R
+++ tests/testthat/test_convert_to_taxa_table.R
@@ -26,4 +26,3 @@ expect_that(convert_to_taxa_table_table(phylum_otu, "class"), throws_error())
 #
 # when wrong taxonomic level is given to convert an otu table
 expect_that(convert_to_taxa_table_table(phylum_otu, "Class"), throws_error())
-#
