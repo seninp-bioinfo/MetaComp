fe74be10ca9c6e46866923694d64a7ff524b633f
Pavel Senin <seninp@gmail.com>
2016-11-01 13:38:56 -0600

    adding gottcha2 readers

--- R/load_gottcha2_assignment.R
+++ R/load_gottcha2_assignment.R
@@ -1,21 +1,22 @@
-
 #' @importFrom data.table fread
 #' @importFrom dplyr select
 NULL
 
-#' Efficiently loads a GOTTCHA (or other EDGE-like taxonomic assignment) table from a file.
-#' An assumption has been made -- since GOTTCHA/EDGE tables are generated in an automated fashion,
+#' Efficiently loads a GOTTCHA2 (or other EDGE-like taxonomic assignment) table from a file.
+#' An assumption has been made -- since GOTTCHA2/EDGE tables are generated in an automated fashion,
 #' they should be properly formatted -- thus the code doesn't check for any inconsistencies except
 #' for the very file existence. This implementation fully relies on the read.table function
 #' from data.table package gaining performance over traditional R techniques.
 #'
-#' @param filepath A path to EDGE-generated tab-delimeted GOTTCHA taxonomy assignment file.
+#' @param filepath A path to EDGE-generated tab-delimeted GOTTCHA2 taxonomy assignment file.
 #'
-#' @return a data frame containing four columns: TAXA, LEVEL, COUNT, and ABUNDANCE, representing
-#'         taxonomically anchored sequences from the sample.
+#' @return a data frame containing four columns: NAME, LEVEL, READ_COUNT, and REL_ABUNDANCE,
+#'         representing taxonomically anchored sequences from the sample.
 #'
 #' @export
-load_gottcha_assignment <- function(filepath) {
+load_gottcha2_assignment <- function(filepath) {
+
+  print(paste(filepath))
 
   TAXA <- LEVEL <- COUNT <- ABUNDANCE <- NULL
 
@@ -35,8 +36,9 @@ load_gottcha_assignment <- function(filepath) {
 
   # rename the abundance column
   #
+  names(df) <- sub("NAME", "TAXA", names(df))
+  names(df) <- sub("REL_ABUNDANCE", "ABUNDANCE", names(df))
   names(df) <- sub("READ_COUNT", "COUNT", names(df))
-  names(df) <- sub("NORM_COV", "ABUNDANCE", names(df))
 
 
   # return results, "as a data frame" to avoid any confusion...
---
+++ R/load_gottcha2_assignments.R
@@ -0,0 +1,41 @@
+#' @importFrom data.table fread
+NULL
+
+#' Efficiently loads a GOTTCHA2 (or other EDGE-like taxonomic assignment) tables from a list
+#' of files. Outputs a named list of assignments.
+#'
+#' @param filepath A path to tab delimeted, two-column file whose first column is a project_id
+#' (which will be used to name this assignment) and the second column is the assignment filename.
+#'
+#' @return a list of all read assignments.
+#'
+#' @export
+load_gottcha2_assignments <- function(filepath) {
+
+  # check for the file existence
+  #
+  if ( !file.exists(filepath) ) {
+    stop(paste("Specified file \"", filepath, "\" doesn't exist!"))
+  }
+
+  # read the file
+  #
+  df <- data.table::fread(filepath, sep = "\t", header = F)
+
+  # read files
+  #
+  input_assignments_list <- list(MetaComp::load_gottcha2_assignment(df[1, ]$V2))
+  if (dim(df)[1] > 1) {
+    for (i in 2:(dim(df)[1])) {
+      input_assignments_list <- c(input_assignments_list,
+                                     list(MetaComp::load_gottcha2_assignment(df[i, ]$V2)))
+    }
+  }
+
+  # name the list
+  #
+  names(input_assignments_list) <- df$V1
+
+  input_assignments_list
+
+}
